6f1997f3815d7e164473765604101e062a814466
Chebo <topbdom@gmail.com>
2026-08-01 01:06:44 +0300

    Add files via upload

---
+++ data/config.js
@@ -0,0 +1,7 @@
+window.SITE_CONFIG = {
+  name: "Моя манхва",
+  // Поки сайт відкривається локально, обкладинки беруться з цього каталогу.
+  // Після завантаження в R2 тут буде: "https://назва.pages.dev/media/titles"
+  mediaBaseUrl: "file:///D:/укр_webp",
+  localPreview: true
+};
