fdee851ad5fe276a5406bf8f0d55eb6bae895edf
singular311 <slavutakom@gmail.com>
2026-09-03 15:09:20 +0300

    Publish updated manga catalog

--- website/data/cubari-links.js
+++ website/data/cubari-links.js
@@ -431,7 +431,8 @@ window.CUBARI_LINKS = {
       "80",
       "81",
       "82",
-      "83"
+      "83",
+      "84"
     ]
   },
   "vsuperech-zanepadu": {
--- website/data/last-updated.js
+++ website/data/last-updated.js
@@ -20,7 +20,7 @@ window.LAST_UPDATED = {
   "absolyutnyi-nekromant": 1788346676,
   "bastard-klanu-mecha-shlyakh-do-vyzhyvannya": 1788351785,
   "vyzhyvannya-nevylikovno-khvoroho-heniya": 1788352564,
-  "vyzhyvannya-u-hri-za-varvara": 1788018101,
+  "vyzhyvannya-u-hri-za-varvara": 1788437360,
   "velykyi-nebesnyi-povelytel-demoniv": 1788260181,
   "aster-buntar-domu-tyraniv": 1787747688,
   "bezsmertnyi-khlopchyk": 1788258099
