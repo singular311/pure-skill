55e3e93c13d5f1475c25e24f0b86c280f543cf6f
singular311 <slavutakom@gmail.com>
2026-09-15 14:53:15 +0300

    Publish updated manga catalog

--- website/data/last-updated.js
+++ website/data/last-updated.js
@@ -1,7 +1,7 @@
 window.LAST_UPDATED = {
   "yedynyi-nekromant": 1789046869,
   "zhnets-dreifuyuchoho-misyatsya": 1789052797,
-  "istoriya-vyzhyvannya-korolya-mecha-u-inshomu-sviti": 1789052336,
+  "istoriya-vyzhyvannya-korolya-mecha-u-inshomu-sviti": 1789473194,
   "koloryst": 1789134403,
   "lytsar-vohnyu": 1789137063,
   "vsuperech-zanepadu": 1789392312,
